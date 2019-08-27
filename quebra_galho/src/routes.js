31b43892049db2c9fb8a78939afc10808fd67d0f
Haaragard <haaragard@gmail.com>
2019-08-26 22:01:04 -0300

    Iniciando projeto app

---
+++ quebra_galho/src/routes.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { createAppContainer, createStackNavigator } from 'react-navigation';
+
+// import { Container } from './styles';
+
+export default class src extends Component {
+  render() {
+    return <View />;
+  }
+}
